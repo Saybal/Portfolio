30654a9b093800b1e05a43185059668db3c6fc06
Saybal <roysaybal@gmail.com>
2025-08-14 11:53:24 +0600

    modified dropdown menu

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -2,9 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-scroll";
 import { useTheme } from "../Hook/useTheme";
 
-
 const Navbar = () => {
-
   const currentTheme = useTheme();
   const [theme, settheme] = useState(
     localStorage.getItem("Theme") ? localStorage.getItem("Theme") : "acid"
@@ -27,50 +25,40 @@ const Navbar = () => {
   const link = (
     <>
       <li className="body-font font-semibold mx-3 text-base-content text-lg cursor-pointer">
-        <Link to="home" smooth={true} duration={600} offset={-80}>Home</Link>
+        <Link to="home" smooth={true} duration={600} offset={-80}>
+          Home
+        </Link>
       </li>
       <li className="body-font font-semibold mx-3 text-base-content text-lg cursor-pointer">
-        <Link to="about" smooth={true} duration={600} offset={-80}>About Me</Link>
+        <Link to="about" smooth={true} duration={600} offset={-80}>
+          About Me
+        </Link>
       </li>
       <li className="body-font font-semibold mx-3 text-base-content text-lg cursor-pointer">
-        <Link to="education" smooth={true} duration={600} offset={-80}>Education</Link>
+        <Link to="education" smooth={true} duration={600} offset={-80}>
+          Education
+        </Link>
       </li>
       <li className="body-font font-semibold mx-3 text-base-content text-lg cursor-pointer">
-        <Link to="projects" smooth={true} duration={600} offset={-80}>Projects</Link>
+        <Link to="projects" smooth={true} duration={600} offset={-80}>
+          Projects
+        </Link>
       </li>
       <li className="block body-font font-semibold mx-3 text-base-content text-lg cursor-pointer">
-        <Link to="contact" smooth={true} duration={1000} offset={-80}>Contact Me</Link>
+        <Link to="contact" smooth={true} duration={1000} offset={-80}>
+          Contact Me
+        </Link>
       </li>
     </>
   );
 
   return (
-    <div className={`navbar bg-base-100 shadow-sm sticky top-0 z-50 ${currentTheme === 'sunset' ? "border-b-1 border-white/15" : ""}`}>
+    <div
+      className={`navbar bg-base-100 shadow-sm sticky top-0 z-50 ${
+        currentTheme === "sunset" ? "border-b-1 border-white/15" : ""
+      }`}
+    >
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
-          >
-            {link}
-          </ul>
-        </div>
         <a className="flex gap-2 btn btn-ghost text-xl">
           <img
             className="w-30 h-12 md:w-40 md:h-14 lg:w-40 lg:h-15"
@@ -107,6 +95,30 @@ const Navbar = () => {
             <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
           </svg>
         </label>
+      </div>
+      <div className="dropdown dropdown-end">
+        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />
+          </svg>
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+        >
+          {link}
+        </ul>
       </div>
     </div>
   );
